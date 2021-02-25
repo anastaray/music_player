43675fb1eefbb48cb3cd4826b474ed1ea8865bad
anastaray <anastaray>
2021-02-25 17:19:22 +0300

    add gitignore file add MainActivity with FrameLayout add SignInFragment and PlaylistsFragment check is User login and show corresponding fragment add SSH key and login with it through google

--- app/src/main/java/com/anazta217/musicplayer/MainActivity.java
+++ app/src/main/java/com/anazta217/musicplayer/MainActivity.java
@@ -1,19 +1,42 @@
 package com.anazta217.musicplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.registration);
 
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
+        if (account == null) {
+            openSignInFragment();
+        } else {
+            openFirstFragment(account);
+        }
+    }
+
+    public void openSignInFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        SignInFragment signInFragment = new SignInFragment();
+        fragmentTransaction.replace(R.id.mainContainer, signInFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void openFirstFragment(GoogleSignInAccount account) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        PlaylistsFragment playlistsFragment = new PlaylistsFragment();
+        playlistsFragment.setText(account.getDisplayName() + " the best");
+        fragmentTransaction.replace(R.id.mainContainer, playlistsFragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/anazta217/musicplayer/SignInFragment.java
@@ -0,0 +1,76 @@
+package com.anazta217.musicplayer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class SignInFragment extends Fragment implements View.OnClickListener {
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 15;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View inflate = inflater.inflate(R.layout.sign_in_fragment, container, false);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+        inflate.findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        return inflate;
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException e) {
+
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+           Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+           handleSignInResult(task);
+        }
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    public void updateUI(GoogleSignInAccount account){
+        ((MainActivity)getActivity()).openFirstFragment(account);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+}
