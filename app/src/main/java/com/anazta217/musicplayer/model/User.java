3a4c0649d143ea6d7f5739362a72a2a41f7e8bde
anastaray <anastaray>
2021-03-11 17:52:43 +0300

    create class User.java

---
+++ app/src/main/java/com/anazta217/musicplayer/model/User.java
@@ -0,0 +1,25 @@
+package com.anazta217.musicplayer.model;
+
+import android.util.Patterns;
+
+public class User {
+
+    private String email;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isEmailValid() {
+        return Patterns.EMAIL_ADDRESS.matcher(getEmail()).matches();
+    }
+
+    public boolean isPasswordLengthGreaterThan5() {
+        return getPassword().length() > 5;
+    }
+}
